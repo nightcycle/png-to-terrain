0d4cb2fafaa984510f74b356e89117029c0240b5
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-01-19 14:31:18 -0700

    Actually accessing relevant properties

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-use std::fs;
 use std::fs::File;
 use std::io::Read;
 
 fn main() {
-	let mut file = match File::open("./test/Test.txt") {
+	let mut file = match File::open("./test/out.xml") {
 		Ok(file) => file,
 		Err(e) => {
-		    println!("Error opening file: {}", e);
-		    return;
+			println!("Error opening file: {}", e);
+			return;
 		}
-	 };
+	};
 
-	 let mut contents = String::new();
-	 match file.read_to_string(&mut contents) {
-		Ok(_) => println!("File contents: {}", contents),
+	let mut contents = String::new();
+	match file.read_to_string(&mut contents) {
+		Ok(_) => println!("File contents"),
 		Err(e) => println!("Error reading file: {}", e),
-	 }
-}
- 
-fn xml_encode_config() -> rbx_xml::EncodeOptions {
-	rbx_xml::EncodeOptions::new().property_behavior(rbx_xml::EncodePropertyBehavior::WriteUnknown)
+	};
+	
+	let model = match rbx_xml::from_str_default(contents) {
+		Ok(model) => model,
+		Err(e) => {
+			println!("Error building model: {}", e);
+			return;
+		}
+	};
+	let root = model.root();
+	let terrain_ref = root.children()[0];
+	let terrain = model.get_by_ref(terrain_ref).unwrap();
+	println!("Class name {}", terrain.class);
+	let physicsGrid = terrain.properties.get("PhysicsGrid");
+	
+	// println!("PhysicsGrid {}", physicsGrid);
 }
