8a35b6fa87beab07268a397a134c6ed5e1aa5545
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-01-19 18:11:39 -0700

    About to swap to place format

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
 use std::io::Read;
-use serde_json::to_string;
+use rbx_types::Variant;
 
 fn main() {
 	// Reads xml file
-	let mut file = match File::open("./test/out.xml") {
+	let mut file = match File::open("./test/input.xml") {
 		Ok(file) => file,
 		Err(e) => {
 			println!("Error opening file: {}", e);
@@ -32,24 +32,22 @@ fn main() {
 	let root = model.root();
 	let terrain_ref = root.children()[0];
 	let terrain = model.get_by_ref(terrain_ref).unwrap();
-	println!("Class name {}", terrain.class);
+	// println!("Class name {}", terrain.class);
 
 	// Get physics properties
-	let physics_grid = match terrain.properties.get("PhysicsGrid") {
-		Some(physics_grid) => {
-		    let physics_grid_json = to_string(physics_grid).unwrap();
-		    let physics_grid_bytes = physics_grid_json.as_bytes();
-		//     println!("PhysicsGrid {}", physics_grid_bytes);
+	for name in terrain.properties.keys() {
+	    let val_ref = match terrain.properties.get(name){
+		Some(val_ref) => {
+			if matches!(val_ref, Variant::BinaryString(_)) {
+				println!("{} = BinaryString", name);
+			};
+	
 		},
 		None => {
-		    println!("PhysicsGrid not found in properties");
+		    println!("{} could not be opened", name);
 		}
 	};
-
-	for name in terrain.properties.keys() {
-	//     let value = properties.get(name).unwrap();
-	    println!("{}", name);
-	}
+}
 	
 
 }
